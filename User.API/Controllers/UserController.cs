547e97e9bf9a33245e0fbcd97b16e6fcec2d9b61
Edu <eduardo@engdigital.com.br>
2020-06-01 16:55:58 -0300

    Estrutura inicial api de usuarios

---
+++ User.API/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.API.Controllers
+{
+    [Route("v1/users")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+
+        [HttpGet]
+        public void GetById()
+        {
+
+        }
+
+        [HttpGet]
+        public void GetByEmail()
+        {
+
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+
+    }
+}
