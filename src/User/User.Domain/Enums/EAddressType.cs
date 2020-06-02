02485ac5951fbaa097367180d0cb243bac4eca79
Edu <eduardo@engdigital.com.br>
2020-06-02 15:04:47 -0300

    Estrtura basica do contexto de usuarios

---
+++ src/User/User.Domain/Enums/EAddressType.cs
@@ -0,0 +1,10 @@
+﻿namespace User.Domain.Enums
+{
+    public enum EAddressType
+    {
+        Shipping = 1, 
+        Billing = 2,
+        Comercial = 3,
+        Persona = 4
+    }
+}
---
+++ src/User/User.Domain/ValueObjects/Email.cs
@@ -0,0 +1,14 @@
+﻿namespace User.Domain.ValueObjects
+{
+    public class Email
+    {
+        public Email(string email)
+        {
+            UserEmail = email;
+        }
+
+        public string UserEmail { get; private set; }
+
+        public override string ToString() => UserEmail.ToString();
+    }
+}
